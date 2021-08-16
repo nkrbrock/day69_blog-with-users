5619453adfb6752d5f8c3acda82dcbbe0a617d9e
nkrbrock <nkrbrock@gmail.com>
2021-08-16 09:56:06 -0500

    add ignore

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 
-gravatar = Gravatar(app, size=100, rating="g", default="retro", force_default=False, force_lower=False, base_url=None)
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+gravatar = Gravatar(app, size=100, rating="g", default="retro", force_default=False, force_lower=False, base_url=None)
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
